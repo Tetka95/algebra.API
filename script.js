6f1664be98216ced352e1222aaadb9de82ce8f29
Tetka95 <martina.bobek95@gmail.com>
2024-05-20 19:26:57 +0200

    ISPRAVITI!!!

--- script.js
+++ script.js
@@ -9,7 +9,26 @@ request.open("GET", "https://xsmilex007.github.io/JS-API-example/mydata.json", t
 //isparsat ćemo JSON, pretvorit ga u objekt te ga prikazati na stranici
 request.onload = function(result){
     console.log(result.currentTarget.response);
+
+    const obj = JSON.parse(result.currentTarget.response);
+    let grad = obj.request.query;
+    console.log("isparsani grad: " + grad);
+    let country = obj.location.country;
+    let regija = obj.location.region
+    
+    let temperatura = obj.current.temparature;
+    let uvIndex = obj.current.uv_index;
+
+    console.log(
+        `Country: ${country}|Regija: ${regija}|Temperatura: ${temperatura}|UV index: ${uvIndex}`
+        );
 };
 
 //iniciramo GET request prema endpointu
 request.send();
+
+document.getElementById("grad").value = grad;
+document.getElementById("country").textContent = country;
+document.getElementById("regija").textContent = regija;
+document.getElementById("temperatura").textContent = temperatura;
+document.getElementById("uvIndex").textContent = uvIndex;
